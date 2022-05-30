0a5464bb03d18f65e49945879355f420d47767cd
Fjborrero178 <fjborrero@uninorte.edu.co>
2022-05-30 17:55:17 -0500

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get("/style", (req, res) => {
+app.get("/style.css", (req, res) => {
     res.sendFile(path.join(__dirname + "/style.css"));
   });
 
