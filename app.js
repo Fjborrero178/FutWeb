b3a0ce634ba63f48a1d41b39ab68213b34232220
AlexGDLH <delahozfa@uninorte.edu.co>
2022-06-03 17:37:28 -0500

    jajaja

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const path = require("path");
 const mysql = require('mysql');
 
 
-
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 con.connect((err) => {
 if (err) {
