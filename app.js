8cc2d5d0e7fb009649d9b53193b49b19bc18e479
Fjborrero178 <fjborrero@uninorte.edu.co>
2022-06-03 17:18:35 -0500

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const path = require("path");
+const mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+con.connect((err) => {
+if (err) {
+    console.error('error conecting: ' + err.stack);
+    return;
+}
+else {
+    console.log("Connected to database.");
+}
+});
+
+
 
 const app = express();
 
